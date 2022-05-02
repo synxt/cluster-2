d610a3d81d61dd123ad44b7addd10d3e3b3852f2
synxt <krishna@synxt.in>
2022-05-02 20:02:58 +0530

    Title Case Conversion Added

--- strings/src/main/java/in/synxt/strings/ChangeCase.java
+++ strings/src/main/java/in/synxt/strings/ChangeCase.java
@@ -7,12 +7,21 @@ public class ChangeCase {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter A Sentence");
 		String inputStr = sc.nextLine();
-		System.out.println("1)Lower Case \n 2)Uppercase");
+		System.out.println("1)Lower Case \n 2)Uppercase \n3)Title Case");
 		int choice = Integer.parseInt(sc.nextLine());
 		if(choice == 1) {
 			inputStr = inputStr.toLowerCase();
-		}else {
+		}else if(choice == 2){
 			inputStr = inputStr.toUpperCase();
+		}else {
+			String[] words = inputStr.split(" ");
+			StringBuilder titleCaseStr = new StringBuilder();
+			for(String word:words) {
+				titleCaseStr.append(Character.toUpperCase(word.charAt(0)))
+							.append(word.substring(1))
+							.append(" ");
+			}
+			inputStr = titleCaseStr.toString();
 		}
 		System.out.println(inputStr);
 		System.out.println("THE END");
