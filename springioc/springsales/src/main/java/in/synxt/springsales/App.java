0a10727c53936b66b6e5aedcf2cebdb7568d229b
Synxt <Synxt>
2022-08-06 20:38:52 +0530

    Spring IOC basics

--- springioc/springsales/src/main/java/in/synxt/springsales/App.java
+++ springioc/springsales/src/main/java/in/synxt/springsales/App.java
@@ -3,15 +3,18 @@ package in.synxt.springsales;
 import java.util.List;
 import java.util.Scanner;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import in.synxt.springsales.model.Product;
 import in.synxt.springsales.service.ProductService;
-import in.synxt.springsales.service.impl.ProductServiceImpl;
 
 public class App {
-	public static void main(String[] args) throws Exception {		
-		ProductService productService = new ProductServiceImpl();
+	public static void main(String[] args) throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		ProductService productService = context.getBean("productService",ProductService.class);
 		List<Product> products = productService.getAllProducts();
-		System.out.println("Please choose a prodct");
+		System.out.println("Please choose a product");
 		int choice = 0;
 		Scanner sc = new Scanner(System.in);
 		for(Product  product: products) {
--- springioc/springsales/src/main/java/in/synxt/springsales/service/impl/ProductServiceImpl.java
+++ springioc/springsales/src/main/java/in/synxt/springsales/service/impl/ProductServiceImpl.java
@@ -9,10 +9,27 @@ import in.synxt.springsales.model.Product;
 import in.synxt.springsales.service.ProductService;
 
 public class ProductServiceImpl implements ProductService{
-	private ProductDao dao = new ProductDaoImpl(); 
+	private ProductDao dao; 
+	
+	public ProductDao getDao() {
+		return dao;
+	}
+
+	public void setDao(ProductDao dao) {
+		this.dao = dao;
+	}
+
+	public ProductServiceImpl() {
+		super();
+	}
+
+	public ProductServiceImpl(ProductDao dao) {
+		super();
+		this.dao = dao;
+	}
 	@Override
 	public List<Product> getAllProducts() throws Exception {
-		List<Product> products = null;
+		List<Product> products = null;		
 		try{
 			products = dao.getAllProducts();
 		}catch(SQLException ex) {
